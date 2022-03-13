f5012cd9ac20fa6bfcfb4cb65acda0e113d8eea0
Elschnagoo <david.nagy@quinto.io>
2022-03-13 18:14:29 +0100

    0.22.0-alpha.0

--- tests/core.test.ts
+++ tests/core.test.ts
@@ -1,87 +1,26 @@
 import * as Path from 'path';
-import {createFolderIfNotExist, removeFolderIfExist, sleep} from "@grandlinex/core";
-import { TestKernel} from './DebugClasses';
-
+import {
+  createFolderIfNotExist,
+  setupDevKernel,
+  TestKernel,
+} from '@grandlinex/core';
+import { ELogger } from '../src';
 
 const appName = 'TestKernel';
 const appCode = 'tkernel';
 const msiPath = Path.join(__dirname, '..', 'data');
 const testPath = Path.join(__dirname, '..', 'data', 'config');
 
-
 createFolderIfNotExist(msiPath);
 createFolderIfNotExist(testPath);
+const kernel = TestKernel.getEntity(
+  new TestKernel(appName, appCode, testPath, __dirname, (k) => new ELogger(k))
+);
 
+setupDevKernel(kernel);
 
-let kernel = new TestKernel(appName, appCode, testPath);
-
-const testText = 'hello_world';
-
-describe('Clean start', () => {
-    test('preload', async () => {
-        expect(kernel.getState()).toBe('init');
-    });
-    test('start kernel', async () => {
-        const result = await kernel.start();
-        expect(result).toBe(true);
-        expect(kernel.getModCount()).toBe(1);
-        expect(kernel.getState()).toBe('running');
-    });
-})
-
-describe('Kernel - Logger', () => {
-    test('log', async () => {
-        kernel.log("log")
-        expect(true).toBeTruthy();
-    });
-    test('warn', async () => {
-        kernel.warn("warn")
-        expect(true).toBeTruthy();
-    });
-    test('error', async () => {
-        kernel.error("error")
-        expect(true).toBeTruthy();
-    });
-    test('debug', async () => {
-        kernel.debug("debug")
-        expect(true).toBeTruthy();
-    });
-})
-describe('KernelModule - Logger', () => {
-    test('log', async () => {
-        kernel.getModule().log("log")
-        expect(true).toBeTruthy();
-    });
-    test('warn', async () => {
-        kernel.getModule().warn("warn")
-        expect(true).toBeTruthy();
-    });
-    test('error', async () => {
-        kernel.getModule().error("error")
-        expect(true).toBeTruthy();
-    });
-    test('debug', async () => {
-        kernel.getModule().debug("debug")
-        expect(true).toBeTruthy();
-    });
-})
-
-
-describe("ShutDown", () => {
-
-    test('exit kernel', async () => {
-        const result = await kernel.stop();
-
-        await sleep(1000);
-
-        expect(kernel.getState()).toBe('exited');
-
-        expect(result).toBeTruthy();
-    });
-
-    test('cleanup', async () => {
-
-        expect(removeFolderIfExist(testPath)).not.toBeFalsy()
-    });
-})
-
+require('@grandlinex/core/dist/dev/lib/start');
+require('@grandlinex/core/dist/dev/lib/core');
+require('@grandlinex/core/dist/dev/lib/dbcon');
+require('@grandlinex/core/dist/dev/lib/end');
+require('@grandlinex/core/dist/dev/lib/orm');
